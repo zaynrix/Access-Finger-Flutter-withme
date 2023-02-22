328961cc85c63579acfff6e209db832395333886
mac <zayns1914@gmail.com>
2023-02-22 10:16:09 +0200

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -201,7 +201,6 @@ class _WelcomePageState extends State<WelcomePage> {
                 ],
               ),
             )
-            // : Center(child: Text("")),
           ],
         ),
       ),
